c54a934bf78725246912175a464aa45ec8f4405e
unknown <lucasmoreiralc@gmail.com>
2023-08-23 14:36:12 -0300

    fix: Github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/BudgetApp",
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    base: "/",
+    plugins: [react()],
+  };
+
+  if (command !== "serve") {
+    config.base = "/BudgetApp/";
+  }
+
+  return config;
 });
